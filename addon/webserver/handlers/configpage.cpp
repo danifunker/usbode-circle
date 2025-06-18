594f05b78d65da4216fd890236bed8c21435b636
Dani Sarfati <dani@funkervogt.com>
2025-06-18 13:41:23 -0400

    resolving issues with config page

--- addon/webserver/handlers/configpage.cpp
+++ addon/webserver/handlers/configpage.cpp
@@ -366,11 +366,13 @@ THTTPStatus ConfigPageHandler::PopulateContext(kainjow::mustache::data& context,
             if (action == "save_reboot") {
                 success_message = "Configuration saved successfully. Rebooting in 3 seconds...";
                 // Schedule a reboot in 3 seconds
-                new CShutdown(ShutdownReboot, 3000);
+                CShutdown *pShutdown = new CShutdown(ShutdownReboot, 3000);
+                pShutdown->Start();
             } else if (action == "save_shutdown") {
                 success_message = "Configuration saved successfully. Shutting down in 3 seconds...";
                 // Schedule a shutdown in 3 seconds
-                new CShutdown(ShutdownHalt, 3000);
+                CShutdown *pShutdown = new CShutdown(ShutdownHalt, 3000);
+                pShutdown->Start();
             } else {
                 success_message = "Configuration saved successfully. Reboot required for changes to take effect.";
             }
