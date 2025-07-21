94befd4acf9e2624d665f06207c44b06dc766b49
Ian Cass <ian@wheep.co.uk>
2025-07-21 10:56:16 +0100

    Update config service to be an actual service with its own run loop

--- addon/configservice/configservice.cpp
+++ addon/configservice/configservice.cpp
@@ -4,30 +4,44 @@
 #include <ctype.h>
 #include <fatfs/ff.h>
 #include "../../src/kernel.h"
+#include <circle/logger.h>
+
+LOGMODULE("configservice");
 
 //TODO: Find a way to delete settings
-// deal with cmdline.txt. Can we just put in config.txt?
 
-static ConfigService* s_instance = nullptr;
-
-ConfigService* ConfigService::GetInstance()
-{
-    if (s_instance == nullptr) {
-        s_instance = new ConfigService();
-    }
-    return s_instance;
-}
+ConfigService *ConfigService::s_pThis = 0;
 
 ConfigService::ConfigService()
 {
+    // I am the one and only!
+    assert(s_pThis == 0);
+    s_pThis = this;
+
     FATFS* fs = CKernel::Get()->GetFileSystem();
     m_properties = new CPropertiesFatFsFile(CONFIG_FILE, fs);
+
+    cmdline.Load("SD:/cmdline.txt");
+    SetName("configservice");
 }
 
 ConfigService::~ConfigService()
 {
     delete m_properties;
-    s_instance = nullptr;
+}
+
+bool ConfigService::GetUSBFullSpeed()
+{
+    const char* val = cmdline.GetValue("usbspeed");
+    if (val != NULL && strcmp(val, "full") == 0)
+	    return true;
+    return false;
+}
+
+void ConfigService::SetUSBFullSpeed(bool value)
+{
+    cmdline.SetValue("usbspeed", value ? "full" : "high");
+    cmdlineIsDirty=true;
 }
 
 const char* ConfigService::GetCurrentImage(const char *defaultValue)
@@ -36,6 +50,7 @@ const char* ConfigService::GetCurrentImage(const char *defaultValue)
      return m_properties->GetString("current_image", defaultValue);
 }
 
+//TODO bounds checking
 unsigned ConfigService::GetDefaultVolume(unsigned defaultValue)
 {
     m_properties->SelectSection("usbode");
@@ -48,6 +63,7 @@ const char* ConfigService::GetLogfile(const char *defaultValue)
     return m_properties->GetString("logfile", defaultValue);
 }
 
+// TODO enum
 const char* ConfigService::GetDisplayHat(const char *defaultValue)
 {
     m_properties->SelectSection("usbode");
@@ -60,37 +76,75 @@ unsigned ConfigService::GetScreenTimeout(unsigned defaultValue)
     return m_properties->GetNumber("screen_timeout", defaultValue);
 }
 
+
 void ConfigService::SetCurrentImage(const char* value)
 {
     m_properties->SelectSection("usbode");
     m_properties->SetString("current_image", value);
-    m_properties->Save();
+    configIsDirty=true;
 }
 
 void ConfigService::SetDefaultVolume(unsigned value)
 {
     m_properties->SelectSection("usbode");
     m_properties->SetNumber("default_volume", value);
-    m_properties->Save();
+    configIsDirty=true;
 }
 
 void ConfigService::SetDisplayHat(const char* value)
 {
     m_properties->SelectSection("usbode");
     m_properties->SetString("displayhat", value);
-    m_properties->Save();
+    configIsDirty=true;
 }
 
 void ConfigService::SetLogfile(const char* value)
 {
     m_properties->SelectSection("usbode");
     m_properties->SetString("logfile", value);
-    m_properties->Save();
+    configIsDirty=true;
 }
 
 void ConfigService::SetScreenTimeout(unsigned value)
 {
     m_properties->SelectSection("usbode");
     m_properties->SetNumber("screen_timeout", value);
-    m_properties->Save();
+    configIsDirty=true;
+}
+
+bool ConfigService::IsDirty() {
+	return configIsDirty || cmdlineIsDirty;
+}
+
+bool ConfigService::Save() {
+	bool ok = true;
+	if (ok && configIsDirty) {
+		ok = m_properties->Save();
+		configIsDirty = false;
+	}
+
+	if (ok && cmdlineIsDirty) {
+		ok = cmdline.Save("SD:/cmdline.txt");
+		cmdlineIsDirty = false;
+	}
+
+	return ok;
+}
+
+void ConfigService::Run(void) {
+    LOGNOTE("Configservice Run Loop entered");
+
+    while (true) {
+            // See if any configuration needs saving. The "Set" method is
+            // called from an interrupt and we can't "Save" to disk in an
+            // interrupt so we need to do it here
+            if (IsDirty()) {
+                    LOGNOTE("Saving configuration");
+                    Save();
+                    LOGNOTE("Saved configuration");
+            }
+
+            CScheduler::Get()->MsSleep(20);
+    }
+
 }
--- addon/configservice/configservice.h
+++ addon/configservice/configservice.h
@@ -3,30 +3,40 @@
 
 #define CONFIG_FILE "config.txt"
 
+#include "cmdline.h"
 #include <Properties/propertiesfatfsfile.h>
+#include <circle/sched/task.h>
 
-class ConfigService
+class ConfigService : public CTask 
 {
 public:
-    static ConfigService* GetInstance();
+    ConfigService();
+    ~ConfigService();
     const char* GetCurrentImage(const char *defaultValue="image.iso");
     unsigned GetDefaultVolume(unsigned defaultValue=255);
     const char* GetDisplayHat(const char *defaultValue="none");
     unsigned GetScreenTimeout(unsigned defaultValue=30);
     const char* GetLogfile(const char *defaultValue="SD:/usbode-logs.txt");
+    bool GetUSBFullSpeed();
 
     void SetLogfile(const char* value);
     void SetCurrentImage(const char* value);
     void SetDefaultVolume(unsigned value);
     void SetDisplayHat(const char* value);
     void SetScreenTimeout(unsigned value);
+    void SetUSBFullSpeed(bool value);
+
+    bool IsDirty();
+
+    void Run(void);
 
 private:
-    ConfigService();
-    ~ConfigService();
-    ConfigService(const ConfigService&);
-    ConfigService& operator=(const ConfigService&);
     CPropertiesFatFsFile* m_properties;
+    CmdLine cmdline;
+    bool cmdlineIsDirty = false;
+    bool configIsDirty = false;
+    static ConfigService *s_pThis;
+    bool Save();
 };
 
 #endif // CONFIG_SERVICE_H
