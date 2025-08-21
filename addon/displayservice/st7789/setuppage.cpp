703a5fd90a5131bd9d60f6df886b7de840ba3178
Dani Sarfati <dani@funkervogt.com>
2025-08-21 14:38:54 -0400

    adjusting timing for sdcards

--- addon/displayservice/st7789/setuppage.cpp
+++ addon/displayservice/st7789/setuppage.cpp
@@ -19,7 +19,7 @@ void ST7789SetupPage::OnEnter() {
     m_ShouldChangePage = false;
     m_refreshCounter = 0;
     m_setupStarted = false;
-    m_statusText = "Init Setup. Wait up to 30s";
+    m_statusText = "Init Setup. Wait 60 seconds";
     Draw();
 }
 
