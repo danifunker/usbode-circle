03966134f5fd14e8ebd1b38bdda32e0320b104b2
Dani Sarfati <dani@funkervogt.com>
2025-08-21 13:50:52 -0400

    updated setup message for user instruction

--- addon/displayservice/st7789/setuppage.cpp
+++ addon/displayservice/st7789/setuppage.cpp
@@ -19,7 +19,7 @@ void ST7789SetupPage::OnEnter() {
     m_ShouldChangePage = false;
     m_refreshCounter = 0;
     m_setupStarted = false;
-    m_statusText = "Initializing setup...";
+    m_statusText = "Initializing setup. Please Wait up to 30 seconds...";
     Draw();
 }
 
