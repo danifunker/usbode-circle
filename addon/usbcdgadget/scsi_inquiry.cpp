fedb423ccb5a175da18686f6aa0a58e4aadf372f
Dani Sarfati <dani@funkervogt.com>
2025-12-09 18:07:52 -0500

    need to sort out TOC generation for higher spec CDROM drives

--- addon/usbcdgadget/scsi_inquiry.cpp
+++ addon/usbcdgadget/scsi_inquiry.cpp
@@ -352,8 +352,8 @@ void SCSIInquiry::FillModePage(CUSBCDGadget *gadget, u8 page, u8 *buffer, int &l
 
             // Capability bits (6 bytes) - dynamic based on media type
             // Byte 0: bit0=DVD-ROM, bit1=DVD-R, bit2=DVD-RAM, bit3=CD-R, bit4=CD-RW, bit5=Method2
-            codepage.capabilityBits[0] = 0x3f; // Support all media types for DVD, else CD only
-            codepage.capabilityBits[1] = 0x37; // All writable types
+            codepage.capabilityBits[0] = 0x00; // Support all media types for DVD, else CD only
+            codepage.capabilityBits[1] = 0x00; // All writable types
             codepage.capabilityBits[2] = 0xf1; // AudioPlay, composite audio/video, digital port 2, Mode 2 Form 2, Mode 2 Form 1
             codepage.capabilityBits[3] = 0x77; // CD-DA Commands Supported, CD-DA Stream is accurate
             codepage.capabilityBits[4] = 0x29; // Tray loading mechanism, eject supported, lock supported
